ef377562342549a745e7a85e1b57e6c3dba6bbd1
nehagb99 <neha.gb99@gmail.com>
2021-04-06 00:49:18 -0400

    Update Team.tsx

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -38,7 +38,7 @@ function Team() {
       <h4>The best work is produced when diverse voices help create it.</h4>
       <p>Our leadership team works together to disrupt the image of stereotypical programmer. Meet our wave-makers!</p>
       {/* iterate through team for the profile. hint: console.log() to see what the data structure looks like and how you can use it */}
-      {console.log(team[0].name + "test")}
+      {console.log(team[0] + "test")}
       {team.map(mem => <Profile name={mem.name} img={mem.headshot.url} position={mem.position}></Profile>)}
       <Profile name="Emily" img="" position="President" />
     </div>
